0a60f4b5483aa43c5df191d2bd3fbb08617f1603
mkbc3 <leonia.klebanov@gmail.com>
2020-01-07 15:24:31 +0200

    1. unittest added for login page.

--- pomdemo/tests/login.py
+++ pomdemo/tests/login.py
@@ -1,28 +1,27 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome(executable_path="../../drivers/chromedriver.exe")
-driver.implicitly_wait(10)
-driver.maximize_window()
-
-driver.get("https://opensource-demo.orangehrmlive.com/")
-
-driver.find_element_by_id("txtUsername").send_keys("Admin")
-
-driver.find_element_by_id("txtPassword").send_keys("admin123")
-
-driver.find_element_by_id("btnLogin").click()
-time.sleep(1)
-driver.find_element_by_id("welcome").click()
-
-driver.find_element_by_link_text("Logout").click()
-
-time.sleep(4)
-driver.close()
-driver.quit()
-print("Test Completed Successfully")
+import unittest
 
 
+class LoginPageTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome(executable_path="../../drivers/chromedriver.exe")
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
 
+    def test_valid_login(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        self.driver.find_element_by_id("btnLogin").click()
+        time.sleep(1)
+        self.driver.find_element_by_id("welcome").click()
+        self.driver.find_element_by_link_text("Logout").click()
+        time.sleep(4)
 
+    @classmethod
+    def tearDownClass(self):
+        self.driver.close()
+        self.driver.quit()
+        print("Test Completed Successfully")
